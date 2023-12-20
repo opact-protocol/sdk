ebbf95b7fb86e11b277f684f907df19849518057
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-12-20 10:08:00 -0300

    Update hash.ts

--- src/kadena/hash.ts
+++ src/kadena/hash.ts
@@ -4,6 +4,7 @@ import { getPoseidonMessageHash } from '../util'
 export const blake = (value: any): string => Pact.crypto.hash(JSON.stringify(value)) as string
 
 export const getKdaMessage = ({
+  another,
   extData,
 }: any) => {
   return getPoseidonMessageHash(extData)
