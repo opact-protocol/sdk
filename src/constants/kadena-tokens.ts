dca687bf3617a9ba406a59a7ef7346e7ba7900af
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2024-01-17 14:49:11 -0300

    feat: update tokens

--- src/constants/kadena-tokens.ts
+++ src/constants/kadena-tokens.ts
@@ -3,7 +3,7 @@ export const kadenaTokens = [
     id: 0,
     symbol: 'KDA',
     name: 'Kadena',
-    icon: 'https://opact.io/svg/kda.svg',
+    icon: 'https://wallet.opact.io/images/tokens/kda.png',
     hash: '21414792165107094930503066755869007218485843369153027361954450892852833682115',
     address: 'coin',
     namespace: {
